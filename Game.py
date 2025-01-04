ed4591e9f667095270cbd079893becef40415116
Your Name <you@example.com>
2025-01-04 20:32:21 +0100

    random bot done, only small edcase fixes needed

--- Game.py
+++ Game.py
@@ -1,6 +1,6 @@
 # Game
-import numpy as np
 from Player import Player
+import numpy as np
 
 
 class Game:
@@ -17,11 +17,9 @@ class Game:
 
         for i in range(1, self.player_amount + 1):
             self.players.append(Player(i))
+        for j in range(self.player_amount+1, self.player_amount + self.bot_amount + 1):
+            self.players.append(Player(j, is_bot = True))
 
-        '''
-        for i in range(1, self.bot_amount + 1):
-            self.players.append(Player(i + self.player_amount, is_bot = True))
-        '''
 
         self.player_to_act = self.players[0]  # Who goes first
         print (f"Starting new game with {len(self.players)} players")
@@ -35,6 +33,9 @@ class Game:
             print(f"Round {self.round}")
 
             self.round_loop()
+            # reset players
+            for player in self.players:
+                player.reset()
 
             game_over = self.check_game_over()
 
@@ -55,42 +56,35 @@ class Game:
         round_over = False
 
         first = self.player_to_act.name - 1
-        last = self.player_to_act.name + self.player_amount - 1
+        last = self.player_to_act.name + len(self.players) - 1
 
         while not round_over:
             for i in range(first, last):
-                name = i % self.player_amount
+                name = i % len(self.players)
                 self.player_to_act = self.players[name]
                  
                 pass_count = 0
                 for player in self.players:
                     if player.pass_bool:
                         pass_count += 1
-
-                if (self.player_to_act.hand != []) or self.player_to_act.pass_bool:
-                    if pass_count == self.player_amount - 1:    
-                        print('Entering flipping phase')          
-                        winner = self.flip_loop()
-                        
-                        # reset players
-                        for player in self.players:
-                            player.reset()
-
-                        if winner:
-                            self.player_to_act.win_round()
-                            print(f'{self.player_to_act.name} won the round')
-                        else:
-                            self.player_to_act.lose_round()
-                            print(f'{self.player_to_act.name} lost the round')
-
-                        round_over = True
-                        break
-                    else: 
-                        self.turn_loop()
+            
+                if pass_count == len(self.players) - 1:             
+                    winner = self.flip_loop()
+                    
+                    if winner:
+                        self.player_to_act.win_round()
+                        print(f'{self.player_to_act.name} won the round')
+                    else:
+                        self.player_to_act.lose_round()
+                        print(f'{self.player_to_act.name} lost the round')
+                    
+                    round_over = True
+                    break
+                else: 
+                    self.turn_loop()
     
     def flip_loop(self):
         print('Flipping cards')
-        global round_over
         global cards_flipped
         # flip own cards
         flowers = 0
@@ -117,41 +111,39 @@ class Game:
         while cards_flipped < self.player_to_act.betsize:
 
             if self.player_to_act.is_bot:
-                action = self.bot_action(self.player_to_act, flip_flag = True)
+                action_type, player_id, action_value = self.bot_action(self.player_to_act, flip_flag = True)
             else:
-                action = input("Enter action for {}: ".format(self.player_to_act.name))
-
-            try:
+                action = input("Enter action for {}: ".format(self.player_to_act.name))            
                 action_type, player_id, action_value = action.split()
 
-                if action_type == "flip":
-                    player_id = int(player_id)
+            if action_type == "flip":
+                player_id = int(player_id)
 
-                    for player in self.players:
-                        if (player.name == player_id) and (player != self.player_to_act):
+                for player in self.players:
+                    if (player.name == player_id) and (player != self.player_to_act):
 
-                            for i in range(0, int(action_value)):
-                                card = player.stack[i]
-                                
-                                if card.suit == 'flower':
-                                    flowers += 1
-                                    print(f'{self.player_to_act.name} flipped a flower')
-                                elif card.suit == 'skull':
-                                    skulls += 1
-                                    print(f'{self.player_to_act.name} flipped a skull')
-                                    return False
+                        for i in range(0, int(action_value)):
+                            card = player.stack[i]
+                            
+                            if card.suit == 'flower':
+                                flowers += 1
+                                print(f'{self.player_to_act.name} flipped a flower')
+                            elif card.suit == 'skull':
+                                skulls += 1
+                                print(f'{self.player_to_act.name} flipped a skull')
+                                return False
 
-                                player.hand.append(card)
-                                player.stack.remove(card)
-                                cards_flipped += 1
-            except:
-                print('Invalid Input')
+                            player.hand.append(card)
+                            player.stack.remove(card)
+                            cards_flipped += 1
+        
+            #print('Invalid Input')
 
         return True
 
     def turn_loop(self):
         player = self.player_to_act
-        print(f"{player.name}'s turn")
+        #print(f"{player.name}'s turn")
 
         valid_action = False
 
@@ -176,7 +168,7 @@ class Game:
             print(player)
             return False
         
-        if action == "pass":
+        if action == "pass" or action == "pass None":
             if betting_phase:
                 player.pass_betting()
                 print(f'{player.name} passed')
@@ -193,7 +185,7 @@ class Game:
                     return True
 
         if action_type == "bet":
-            if cards_played >= self.player_amount:    
+            if cards_played >= len(self.players):    
                 betsize = int(action_value)
                 if highest_bet < betsize <= cards_played:
                     player.bet(betsize)
@@ -203,7 +195,7 @@ class Game:
                     print(f'{player.name} bet {betsize}')
                     return True
 
-        print("Invalid action")
+        #print("Invalid action")
         return False
         
     def check_game_over(self):
@@ -217,7 +209,7 @@ class Game:
             if player.hand == []:
                 out_of_cards_count += 1
 
-        if out_of_cards_count == self.player_amount - 1:
+        if out_of_cards_count == len(self.players) - 1:
             for player in self.players:
                 if player.hand != []:
                     self.winner = player
@@ -238,26 +230,39 @@ class Game:
             player_ids = np.delete(player_ids, player.name - 1)
             player_id = np.random.choice(player_ids)
 
-            global cards_flipped
-            flip_amount = np.arange(1, self.betsize - cards_flipped)
+
+            flip_amount = np.arange(1, player.betsize - cards_flipped + 1)
             action_value = np.random.choice(flip_amount)
 
             return action_types[0], player_id, action_value
         
         else:
-            action_types = {
-                0: "play",
-                1: "bet",
-                2: "pass"
-            }
-            play_values = {player.hand[i].suit for i in range(len(player.hand))}               
-            bet_values = np.arange(1, len(self.players)*4)
+            if cards_played < len(self.players):
+                action_types = {
+                    0: "play"
+                }
+                play_values = {player.hand[i].suit for i in range(len(player.hand))}               
+            elif betting_phase:
+                action_types = {
+                    0: "bet",
+                    1: "pass"
+                }
+                bet_values = np.arange(1, cards_played)
+            else:
+                action_types = {
+                    0: "play",
+                    1: "bet",
+                }
+                play_values = {player.hand[i].suit for i in range(len(player.hand))}               
+                bet_values = np.arange(1, cards_played)
 
             action_type = np.random.choice(list(action_types.values()))
             if action_type == "play":
                 action_value = np.random.choice(list(play_values))
             elif action_type == "bet":
                 action_value = np.random.choice(bet_values)
+            else:
+                action_value = None
             
             return action_type, action_value
 
